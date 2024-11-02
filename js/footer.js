dc92da879e13ad114a09c95c77324880a5b51111
nabin--raj <gith60707@gmail.com>
2024-11-02 13:01:02 +0545

    2081-07-17D

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,10 @@
 
+const addreshElm = document.querySelector("#addresh-text");
+const addresh = "Bedkot 5 Dharampur<br> Mahendranagar <br>Kanchanpur , Nepal";
+
+addreshElm.innerHTML = addresh;
+
+
 //Whine user click the location icon the window page caange in to location url.....
 const locationIcon = document.getElementById("location-icon");
 const locationUrl = "https://maps.app.goo.gl/hYdB3rLdcPUSuXCh9";
@@ -6,3 +12,4 @@ const locationUrl = "https://maps.app.goo.gl/hYdB3rLdcPUSuXCh9";
 locationIcon.addEventListener("click" , function(e){
     window.open(locationUrl , "_blank");
 });
+
